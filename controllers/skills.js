1600cc869761b2acb00ac2b3e34eafd708c659cd
ade-t <ad_elyn@hotmail.com>
2023-03-02 20:11:25 +0800

    add header and footer .ejs

--- controllers/skills.js
+++ controllers/skills.js
@@ -7,12 +7,14 @@ module.exports = {
   
   function index(req, res) {
     res.render('skills/index', {
-      skills: devSkills.getAll()
+      skills: devSkills.getAll(),
+      title: 'All Dev Skills'
     });
   }
 
   function show(req, res) {
     res.render('skills/show', {
       skill: devSkills.getOne(req.params.id),
+      title: 'Dev-Skills Details'
     });
   }
